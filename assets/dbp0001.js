511306b8134fe579f9b6daca3f89da2e789bb276
Dave Berry <berrydp@gmail.com>
2021-07-25 16:04:03 -0700

    Removed history, due to sensitive data. This is the public snapshot of the deployed app.

---
+++ assets/dbp0001.js
@@ -0,0 +1,8 @@
+// var d = new Date();
+// document.getElementById("h2date").innerHTML = d.toString();
+//
+// var d = new Date();
+// document.getElementById("h2date").innerHTML = "Last updated on: " + d.toLocaleString("en-US", {timeZone: "America/New_York"}) + " (EST)";
+
+// var dg = {{ date_var.v }};
+// document.getElementById("h2date").innerHTML = "Last updated on: " + dg;
